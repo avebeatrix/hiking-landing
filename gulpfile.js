1d8c85d922c55a4f975c43fd4c17714f24b30cde
Beatrix_Kido <la_katalina_s@mail.ru>
2022-05-01 22:58:36 +0300

    color schemes, gulp split desktop & mobile

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,35 @@
 var gulp = require('gulp');
  
-const webp = require('gulp-webp');
-
 const gulpAvif = require('gulp-avif');
-
-const svgo = require('gulp-svgo');
-
 gulp.task('avif', ()=>{
     return gulp.src('i/*.{png,jpg}')
         .pipe(gulpAvif())
         .pipe(gulp.dest('i'));
 });
 
+const webp = require('gulp-webp');
 gulp.task('webp', function () {
     return gulp.src('i/*.jpg')    
         .pipe(webp())
         .pipe(gulp.dest('i'))   
 });
 
+const svgo = require('gulp-svgo');
 gulp.task('svgo', () => { 
     return gulp.src('i/*.svg')
         .pipe(svgo())
         .pipe(gulp.dest('i'));
+});
+
+const mediaQueriesSplitter = require('gulp-media-queries-splitter');
+gulp.task('split-css', () => {
+    return gulp.src('css/styles.css')
+        .pipe(mediaQueriesSplitter([            
+            // Include only CSS rules without screen size based media queries
+            {media: 'none', filename: 'mobile.css'},            
+ 
+            // Include CSS rules for bigger screen sizes (mostly used on desktop)
+            {media: {min: '768px'}, filename: 'desktop.css'},
+        ]))
+        .pipe(gulp.dest('css'))
 });
